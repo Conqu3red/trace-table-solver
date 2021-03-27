09d9e454b6e7e2db9f59291b1c2e473a4133e90e
Conqu3red <Conqu3red@users.noreply.github.com>
2021-03-27 11:30:12 +0000

    initial commit

---
+++ example.py
@@ -0,0 +1,23 @@
+from tracker import *
+
+
+tracker = Tracker()
+t = VarContainer(tracker.onchange)
+
+def _print(msg, *args, **kwargs):
+    t.output = msg
+    print(msg, *args, **kwargs)
+
+t.turns = 0
+t.x = 3
+
+while t.turns < 22:
+    t.x = t.x * 3
+    t.turns = t.turns + 3
+
+_print(t.turns)
+
+_print(t.x)
+
+
+tracker.displayTraceTable()
---
+++ tracker.py
@@ -0,0 +1,41 @@
+from tabulate import tabulate
+from typing import *
+
+class VarContainer:
+    def __init__(self, onchange):
+        super(VarContainer, self).__setattr__("onchange", onchange)
+
+    def __setattr__(self, name, value):
+        #print(f"Setting {name} to {value}")
+        self.onchange(name, value)
+        super(VarContainer, self).__setattr__(name, value)
+
+class Tracker:
+    def __init__(self):
+        self.values = {}
+        self.line = 0
+    
+    def onchange(self, name, value):
+        #print(f"{name} -> {value}")
+        data: dict = self.values.get(name, {})
+        
+        #while self.line in data.keys():
+        self.line += 1
+        
+        data[self.line] = value
+
+        self.values[name] = data
+    def displayTraceTable(self, variables: List[str] = None):
+        headers = list(self.values.keys())
+        if variables:
+            headers = variables
+        
+        maxLine = max([max(l.keys()) for l in self.values.values()])
+        
+        table = []
+        for i in range(maxLine+1):
+            row = []
+            for h in headers:
+                row.append(self.values[h].get(i, ""))
+            table.append(row)
+        print(tabulate(table, headers=headers, tablefmt="psql"))
